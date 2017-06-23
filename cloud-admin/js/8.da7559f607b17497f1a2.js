c6be541c469a96a00ea8f98a68d128f77f34f63a
wentao978 <476788978@qq.com>
2017-06-23 16:59:05 +0800

    add

--- cloud-admin/js/8.da7559f607b17497f1a2.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([8],{231:function(e,n,r){var t=r(30)(null,r(336),null,null,null);e.exports=t.exports},336:function(e,n){e.exports={render:function(){var e=this,n=e.$createElement;return(e._self._c||n)("router-view")},staticRenderFns:[]}}});
-//# sourceMappingURL=8.da7559f607b17497f1a2.js.map
