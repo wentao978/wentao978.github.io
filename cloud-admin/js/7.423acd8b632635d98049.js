b3ac2862dd0a43a837a1f1833da9318a697e8318
wentao978 <476788978@qq.com>
2017-07-06 11:04:51 +0800

    add i18n

---
+++ cloud-admin/js/7.423acd8b632635d98049.js
@@ -0,0 +1,2 @@
+webpackJsonp([7],{277:function(e,n,r){var t=r(36)(null,r(482),null,null,null);e.exports=t.exports},482:function(e,n){e.exports={render:function(){var e=this,n=e.$createElement;return(e._self._c||n)("router-view")},staticRenderFns:[]}}});
+//# sourceMappingURL=7.423acd8b632635d98049.js.map
